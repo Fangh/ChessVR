02d9648c6f08428579baed8e1639679e1d41369d
Thomas 6 Freedom <thomas.klein@6freedom.studio>
2020-04-19 17:29:57 +0200

    V1.0

--- Assets/Scripts/MonoBehaviour/FingerTip.cs
+++ Assets/Scripts/MonoBehaviour/FingerTip.cs
@@ -7,11 +7,14 @@ public class FingerTip : MonoBehaviour
     internal EFinger finger;
     internal FingerTipsManager manager;
 
+    private bool isPushed = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.GetComponent<FingerInteractable>())
+        if (other.GetComponent<FingerInteractable>() && !isPushed)
         {
             other.GetComponent<FingerInteractable>().OnFingerEnter?.Invoke(finger);
+            isPushed = true;
         }
         else if (other.GetComponent<FingerTip>())
         {
@@ -26,9 +29,10 @@ public class FingerTip : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.GetComponent<FingerInteractable>())
+        if (other.GetComponent<FingerInteractable>() && isPushed)
         {
             other.GetComponent<FingerInteractable>().OnFingerExit?.Invoke(finger);
+            isPushed = false;
         }
         else if (other.GetComponent<FingerTip>())
         {
